f1e9e7ac8f1b9405313efaded8de045993447bba
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-13 12:21:23 +0530

    Created 1st component

--- chapter1/basic-react-app/src/App.jsx
+++ chapter1/basic-react-app/src/App.jsx
@@ -1,10 +1,46 @@
 import './App.css'
 
+function Title(){
+  return(
+    <h1>Hello I am Title!</h1>
+  )
+}
+
+function Description(){
+  return(
+    <h3>Hello I am Description!</h3>
+  )
+}
+
+// function App() {
+//   return(
+//     // <h1>Hello World</h1>
+//     // <p>Hello World</p>
+//     <button>HelloReactJS</button>
+//   )
+// }
+
+// function App() {
+//   return(
+//     <div>
+//       <h1>Hello World</h1>
+//       <p>Hello World</p>
+//       <button>HelloReactJS</button>
+//     </div>
+//   )
+// }
+
 function App() {
   return(
-    // <h1>Hello World</h1>
-    // <p>Hello World</p>
-    <button>HelloWorld</button>
+    <div>
+      {/* <Title></Title> */}
+      <Title/>
+      <Description></Description>
+      <Title/>
+      <Description></Description>
+      <Title/>
+      <Description></Description>
+    </div>
   )
 }
 
