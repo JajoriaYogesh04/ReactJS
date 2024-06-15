60b9eddb51aacdb41acb0922c170b256a496211b
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-15 09:31:52 +0530

    Handling Non-Click Events

--- chapter1/basic-react-app/src/Button.jsx
+++ chapter1/basic-react-app/src/Button.jsx
@@ -6,12 +6,24 @@ function printBye(){
     console.log("BYE!");
 }
 
+function handleMouseOver(){
+    console.log("VERY GOOD");
+}
+
+function handleKeyPress(){
+    console.log("Key Pressed")
+}
+
 function Button(){
     return(
         <>
-            <button onClick={printHello}>Hello</button>
+            <button onClick={printHello} >Hello</button>
             <br /><br />
             <button onClick={printBye}>Bye</button>
+            <br /><br />
+            <p onMouseOver={handleMouseOver}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis qui nisi fugiat, eveniet at laboriosam porro? Dicta dolorum sunt beatae necessitatibus adipisci accusamus quia enim at. Dolor facere officiis perferendis.</p>
+            <br /><br />
+            <button onKeyDown={handleKeyPress}>Key press</button>
         </>
     )
 }
