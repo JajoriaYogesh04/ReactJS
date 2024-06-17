4eb3dfea8906be5367c247ade1ddc9ff731af387
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-17 10:04:09 +0530

    Understandig more about state

--- chapter3/react-state-class/src/Counter.jsx
+++ chapter3/react-state-class/src/Counter.jsx
@@ -1,22 +1,28 @@
 import { useState } from "react";
 
+function init(){
+    console.log("Random number Initialized");
+    return Math.random();
+}
+
 function Counter(){
     // let count = 0;
-    let [count, setCount] = useState(0);
+    let [count, setCount] = useState(init());
     // console.log(arr);
     console.log(`Outer: ${count}`)
 
     function increaseCount(){
         setCount((currCount)=>{
-            console.log(`Current1: ${currCount}`);
+            // console.log(`Current1: ${currCount}`);
             return currCount+1;
         });
         // console.log(`Inner1: ${count}`);
-        setCount((currCount)=>{
-            console.log(`Current2: ${currCount}`);
-            return currCount+1;
-        });
+        // setCount((currCount)=>{
+        //     console.log(`Current2: ${currCount}`);
+        //     return currCount+1;
+        // });
         // console.log(`Inner2: ${count}`);
+        // setCount(25);
     }
     return(
         <>
