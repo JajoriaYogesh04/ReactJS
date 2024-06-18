47fbf7d90a2b757e506438d266a22d72c9027b07
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-18 14:13:40 +0530

    Mark as Done functionality for one and all

--- chapter4/react-state-class2/src/Todo.jsx
+++ chapter4/react-state-class2/src/Todo.jsx
@@ -3,7 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 // uuidv4();
 
 function Todo(){
-    let [todos, setTodos]= useState([{task: "TASK", id: uuidv4()}]);
+    let [todos, setTodos]= useState([{task: "Task", id: uuidv4(), isDone: false}]);
     let [newTodo, setNewtodo]= useState("");
 
     function updateNewtodo(event){
@@ -15,7 +15,7 @@ function Todo(){
     function updateTodo(){
         // console.log("Adding new Task")
         setTodos((prevTodo)=>{
-            return [...prevTodo, {task: newTodo, id: uuidv4()}]
+            return [...prevTodo, {task: newTodo, id: uuidv4(), isDone: false}]
         });
         setNewtodo("");
     }
@@ -55,12 +55,34 @@ function Todo(){
         })
     }
 
+    function markAllDone(){
+        // console.log("All Done");
+        // let copy= todos.map((todo)=>({...todo, isDone: true}))
+        // console.log(copy);
+        setTodos((prevTodo)=>{
+            return prevTodo.map((todo)=>({...todo, isDone: true}));
+        })
+    }
+
+    function markOneDone(id){
+        setTodos((prevTodo)=>{
+            return prevTodo.map((todo)=>{
+                if(todo.id === id){
+                    return {...todo, isDone: true};
+                }
+                else{
+                    return todo;
+                }
+            });
+        })
+    }
+
     return(
         <>
             <h2>TO-DO List</h2>
             <input type="text" placeholder="I will do..." onChange={updateNewtodo} value={newTodo} style={{width: "20rem", height: "3rem", borderRadius: "15px", fontSize: "20px", paddingLeft: "3rem"}}/>
             <br /><br />
-            <button onClick={updateTodo}>Add Task</button>
+            <button onClick={updateTodo} style={{backgroundColor:"blue"}}>Add Task</button>
             <br />
             <ul>
                 {
@@ -68,9 +90,13 @@ function Todo(){
                         return(
                             <li key={todo.id}>
                                 <span>
-                                    {todo.task}&nbsp;&nbsp;
-                                    <button onClick={()=>{deleteTodo(todo.id)}} style={{marginBottom:"0.5rem", fontSize:"12px", marginLeft:"1rem", borderRadius:"20px"}}>Delete</button>
+                                    <span style={{textDecoration: todo.isDone ? "line-through" : ""}}>
+                                        {todo.task}
+                                    </span>
+                                    &nbsp;&nbsp;
+                                    <button onClick={()=>{deleteTodo(todo.id)}} style={{marginBottom:"0.5rem", fontSize:"12px", marginLeft:"1rem", borderRadius:"20px", backgroundColor:"red"}}>Delete</button>
                                     <button onClick={()=>{lowerCaseOne(todo.id)}} style={{marginBottom:"0.5rem", fontSize:"12px", marginLeft:"1rem", borderRadius:"20px"}}>Lowercase</button>
+                                    <button onClick={()=>{markOneDone(todo.id)}} style={{marginBottom:"0.5rem", fontSize:"12px", marginLeft:"1rem", borderRadius:"20px", backgroundColor:"green"}}>Mark as done</button>
                                 </span>
                             </li>
                         ) 
@@ -79,6 +105,7 @@ function Todo(){
             </ul>
             <br />
             <button onClick={upperCaseAll} style={{fontSize:"15px", borderRadius:"0px"}}>To UPPERCASE</button>
+            <button onClick={markAllDone} style={{fontSize:"15px", borderRadius:"0px", marginLeft:"1rem", backgroundColor:"green"}}>All Done</button>
         </>
     )
 }
