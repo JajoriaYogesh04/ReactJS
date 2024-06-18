483352667c3f84e187fd62b8305f238c5d13ade2
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-18 09:49:44 +0530

    Adding Unique key for Tasks

--- chapter4/react-state-class2/src/Todo.jsx
+++ chapter4/react-state-class2/src/Todo.jsx
@@ -1,32 +1,35 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from 'uuid';
+// uuidv4();
 
 function Todo(){
-    let [todos, setTodos]= useState(["sample"]);
+    let [todos, setTodos]= useState([{task: "TASK", id: uuidv4()}]);
     let [newTodo, setNewtodo]= useState("");
 
     function updateNewtodo(event){
+        // console.log(event);
         // console.log(event.target.value);
         setNewtodo(event.target.value);
     }
 
     function updateTodo(){
         // console.log("Adding new Task")
-        setTodos([...todos, newTodo]);
+        setTodos([...todos, {task: newTodo, id: uuidv4()}]);
         setNewtodo("");
     }
 
     return(
         <>
+            <h2>TO-DO List</h2>
             <input type="text" placeholder="I will do..." onChange={updateNewtodo} value={newTodo} style={{width: "20rem", height: "3rem", borderRadius: "15px", fontSize: "20px", paddingLeft: "3rem"}}/>
             <br /><br />
             <button onClick={updateTodo}>Add Task</button>
             <br />
-            <h2>TO-DO List</h2>
             <div>
                 <ul>
                     {
                         todos.map((todo)=>{
-                            return <li>{todo}</li>
+                            return <li key={todo.id}>{todo.task}</li>
                         })
                     }
                 </ul>
