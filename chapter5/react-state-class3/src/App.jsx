45b427c42ffe641d637d73b0863b7e0f1fb70c63
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-19 14:14:18 +0530

    Final styling of App.css

--- chapter5/react-state-class3/src/App.jsx
+++ chapter5/react-state-class3/src/App.jsx
@@ -10,14 +10,20 @@ import Ticket from './Ticket'
 function App() {
 
   return (
-    <>
+    <div className='lottery-game'>
       {/* <LotteryGame1></LotteryGame1> */}
       {/* <LotteryGame2></LotteryGame2> */}
       {/* <Ticket ticket={[1,2,3,5,6,7,8]}></Ticket> */}
+      <LotteryGame n={1} total={5}></LotteryGame>
+      <LotteryGame n={2} total={10}></LotteryGame>
+      <LotteryGame n={3} total={15}></LotteryGame>
+      <LotteryGame n={4} total={20}></LotteryGame>
       <LotteryGame n={5} total={25}></LotteryGame>
+      <LotteryGame n={6} total={30}></LotteryGame>
+      <LotteryGame n={7} total={35}></LotteryGame>
       <LotteryGame n={8} total={40}></LotteryGame>
-      <LotteryGame></LotteryGame>
-    </>
+      <LotteryGame n={9} total={45}></LotteryGame>
+    </div>
   )
 }
 
