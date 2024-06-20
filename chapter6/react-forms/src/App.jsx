44953f1149b9766a53d409c9c8f6982ae7ec8e43
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-20 09:11:32 +0530

    Forms in React

--- chapter6/react-forms/src/App.jsx
+++ chapter6/react-forms/src/App.jsx
@@ -1,11 +1,22 @@
 
+import { useState } from 'react'
 import './App.css'
 
 function App() {
+  let [name, setName]= useState("");
+
+  let changeName=(event)=>{
+    // console.log(event.target.value);
+    setName(event.target.value)
+  }
 
   return (
     <>
       <h1>REACT FORMS</h1>
+      <form>
+        <input type="text" value={name} onChange={changeName} placeholder='Enter your name' style={{height:"2rem", width:"25rem"}}/><br/><br/>
+        <button>Submit</button>
+      </form>
     </>
   )
 }
