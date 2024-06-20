2c85e591cfddcefd221793f856efbeddddc2e98c
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-20 11:18:37 +0530

    Multiple Form Inputs

--- chapter6/react-forms/src/Form.jsx
+++ chapter6/react-forms/src/Form.jsx
@@ -1,19 +1,26 @@
 import { useState } from 'react'
 
 export default function Form(){
+    let [username, setUsername]= useState("");
     let [name, setName]= useState("");
 
     let changeName=(event)=>{
         // console.log(event.target.value);
         setName(event.target.value)
     }
+    let changeUsername=(event)=>{
+        // console.log(event.target.value);
+        setUsername(event.target.value)
+    }
 
     return (
         <>
         <h1>REACT FORMS</h1>
         <form>
+            <label htmlFor="name">Name: </label>
+            <input type="text" value={name} onChange={changeName} id='name' placeholder='Enter your name' style={{height:"1.5rem", width:"25rem"}}/><br/><br/>
             <label htmlFor="username">Username: </label>
-            <input type="text" value={name} onChange={changeName} id='username' placeholder='Enter your name' style={{height:"1.5rem", width:"25rem"}}/><br/><br/>
+            <input type="text" value={username} onChange={changeUsername} id='username' placeholder='Enter your username' style={{height:"1.5rem", width:"25rem"}}/><br/><br/>
             <button>Submit</button>
         </form>
         </>
