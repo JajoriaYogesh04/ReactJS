a7806ea5223fecc5af0b950bc7dcac157f1cdf19
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-21 14:11:38 +0530

    Setup Weather API

--- minorproject/react-minor-project/src/SearchBox.jsx
+++ minorproject/react-minor-project/src/SearchBox.jsx
@@ -7,6 +7,24 @@ import { useState } from 'react';
 export default function SearchBox(){
     let [city, setCity]= useState("");
 
+    const API_URL="https://api.openweathermap.org/data/2.5/weather";
+    const API_KEY="b4dafafe7b9d15d910f33d432aeff051";
+
+    let getWeatherInfo= async()=>{
+        let response= await fetch(`${API_URL}?q=${city}&appid=${API_KEY}&units=metric`);
+        let jsonResponse= await response.json();
+        console.log(jsonResponse);
+        let weatherInfo= {
+            temp: jsonResponse.main.temp,
+            temp_min: jsonResponse.main.temp_min,
+            temp_max: jsonResponse.main.temp_max,
+            humidity: jsonResponse.main.humidity,
+            feels_like: jsonResponse.main.feels_like,
+            description: jsonResponse.weather[0].description,
+        }
+        console.log(weatherInfo);
+    }
+
     let handleCity=(event)=>{
         // console.log(event.target.value)
         setCity(event.target.value)
@@ -14,6 +32,7 @@ export default function SearchBox(){
     let handleSubmit=(event)=>{
         event.preventDefault();
         console.log(city);
+        getWeatherInfo();
         setCity("");
     }
 
