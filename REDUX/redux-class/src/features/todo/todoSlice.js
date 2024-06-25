c0745dc4ada29f927347bc9f5a5eae85f89dac6f
JajoriaYogesh04 <166348895+JajoriaYogesh04@users.noreply.github.com>
2024-06-25 09:00:10 +0530

    Created Slice Reducers

--- REDUX/redux-class/src/features/todo/todoSlice.js
+++ REDUX/redux-class/src/features/todo/todoSlice.js
@@ -1,7 +1,7 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { Tuple, createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState= {
-    todo: [{
+    todos: [{
         id: "123",
         task: "Task",
         isDone: false,
@@ -11,5 +11,25 @@ const initialState= {
 export const todoSlice= createSlice({
     name: "todoSlice",
     initialState,
-    reducers: {}
+    reducers: {
+        addTodo: (state, action)=>{
+            let newTodo={
+                id: nanoid(),
+                task: action.payload,
+                isDone: true,
+            }
+            state.todos.push(newTodo);
+        },
+        deleteTodo: (state, action)=>{
+            state.todos.filter((todo)=> todo.id !== action.payload )
+        },
+        markasDone: (state, action)=>{
+            state.todos.map((todo)=>{
+                if(todo.id === action.payload){
+                    todo.isDone = true;
+                }
+            })
+        }
+    }
 })
+
